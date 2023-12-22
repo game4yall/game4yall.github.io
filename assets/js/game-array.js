46b1ea76a7aa36b5eee1d9427d5408931ff94a5a
rboy79 <hongsun3010@gmail.com>
2023-12-22 20:37:19 +0700

    1

---
+++ assets/js/game-array.js
@@ -0,0 +1,92 @@
+const gameList = [
+  {
+    name: "Xmas Dash",
+    id: "xmas-dash",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Daily Sudoku 2",
+    id: "daily-sudoku-2",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Temple Run",
+    id: "temple-run",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Bomb 2",
+    id: "bomb-2",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Santa Run",
+    id: "santa-run",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Dubble 5",
+    id: "Dubble-5",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Hexa",
+    id: "hexa",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Fishing Hole",
+    id: "fishing-hole",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Running Fred",
+    id: "running-fred",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+  {
+    name: "Dancing Line",
+    id: "dancing-line",
+    image:
+      "https://img.gamedistribution.com/dd9701cd84da40699cdc404645f29c1f-512x384.jpg",
+    link: "https://html5.gamedistribution.com/196df5aadad34353b14a5d283515e2b3/",
+    description:
+      "Buckle up for a festive ride with Xmas Dash, a captivating rhythm-based game that throws you headfirst into the heart of Christmas cheer. Inspired by the legendary Geometry Dash, this holiday-themed spin-off brings a whole new layer of joy to the classic gameplay. Say goodbye to the plain old square icon, Xmas Dash introduces seven delightful Christmas skins to liven up your journey.",
+  },
+];
