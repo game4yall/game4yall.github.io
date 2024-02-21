8c2297f4d00a6357f1007f265e0a3458ef710e46
rboy79 <hongsun3010@gmail.com>
2024-02-21 19:59:54 +0700

    1

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -7,12 +7,11 @@ document.addEventListener("DOMContentLoaded", async function () {
   const game = document.getElementById("search-result");
 
   renderGames(game, data.items);
-
-  const search = document.querySelector(".form-control.search");
-  search.addEventListener("input", function (event) {
-    const searchValue = event.target.value.toLowerCase(); // Convert search value to lowercase
+  const search = document.getElementsByClassName("form-control search");
+  search.addEventListener("keydown", function (event) {
+    const searchValue = event.target.value;
     const filteredGames = data.items.filter((game) => {
-      return game.name.toLowerCase().includes(searchValue);
+      return game.name.toLowerCase().includes(searchValue.toLowerCase());
     });
     renderGames(game, filteredGames);
   });
@@ -20,27 +19,38 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 function renderGames(gameDiv, gamesData) {
   let innerHTML = "";
-  gamesData.forEach((game) => {
+  gamesData.array.forEach((game) => {
     innerHTML += `
+
     <div class="section-title">
-      <h3>Search Results</h3>
-    </div>
-    <div class="row listing" id="listing1">
-      <div class="col-xl-2 col-lg-3 col-md-4 col-6 grid-1">
-        <a href="/games/${game.gameid}.html">
-          <div class="game-item">
-            <div class="list-game new">
-              <div class="list-thumbnail">
-                <img src="${game.img}.jpg" class="small-thumb lazyload">
-              </div>
-              <div class="list-info">
-                <div class="list-title ellipsis">${game.name}</div>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>`;
+						<h3>
+							 Search Results
+						</h3>
+					</div>
+					<div class="row listing" id="listing1">
+
+						<div class="col-xl-2 col-lg-3 col-md-4 col-6 grid-1">
+							<a href="/games/"${game.gameid}".html">
+								<div class="game-item">
+									<div class="list-game new">
+										<div class="list-thumbnail">
+											<img src=""${game.img}".jpg"
+												class="small-thumb lazyload">
+										</div>
+										<div class="list-info">
+											<div class="list-title ellipsis">"${game.name}"</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+
+					
+
+
+					</div>
+
+        `;
   });
   gameDiv.innerHTML = innerHTML;
 }
